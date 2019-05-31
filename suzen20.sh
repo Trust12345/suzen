d545cca3755113d0efe71f14248b889566261dc9
Trust12345 <alexandrshirokov2000@mail.ru>
2019-05-31 18:02:19 +0300

    nineth commit

---
+++ suzen20.sh
@@ -0,0 +1,36 @@
+#!bin/bash/
+cd ..
+ls
+cd john/
+ls
+cd Desktop/
+ls
+cd films/
+ls
+cd ololo/
+ls
+cd ..
+cd ..
+ls
+cd films/
+rmdir ololo/
+cd ..
+rmdir music/
+cd ..
+rmdir Desktop/
+rm file1
+rm file2
+rm file3
+cd Documents/
+cd music/
+rmdir azaza/
+cd ..
+rmdir music/
+cd films/
+rmdir azaza/
+cd ..
+rmdir films/
+cd ..
+rmdir Documents/
+cd ..
+cd suzen/
